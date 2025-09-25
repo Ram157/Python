dca4d685fb7c7a5298d585151470a1b2f8a9c306
Ram157 <133510419+Ram157@users.noreply.github.com>
2025-09-25 19:36:45 +0500

    Update 2 dz.py

--- 2 dz.py
+++ 2 dz.py
@@ -1,7 +1,10 @@
 from random import *
 from string import *
 print('Введите длину пароля: ')
-len=input()
-lett = ''.join(choice(ascii_uppercase) for _ in range(3))
-digit=''.join(choice(digits) for _ in range(3))
-spec=''.join(choice(digits) for _ in range(2))
+spec_symbols='!@#$%^&*()'
+len=int(input())
+passs = ''.join(choice(ascii_uppercase+digits+spec_symbols) for _ in range(len))
+passs=list(passs)
+shuffle(passs)
+passs=''.join(passs)
+print(passs)
