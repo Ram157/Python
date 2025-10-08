9afbd4cfa4f98891499bc3c7e1741ebdceea30dd
Ram157 <133510419+Ram157@users.noreply.github.com>
2025-10-08 14:57:41 +0500

    1

--- practice 5.py
+++
@@ -1,7 +0,0 @@
-while True:
-    print('Введите слово, которое загадываете(5 букв)')
-    word=input()
-    if len(word)!=5 or isalpha: continue
-    print('Введите возможную букву')
-    InWord=input()
-    if 
